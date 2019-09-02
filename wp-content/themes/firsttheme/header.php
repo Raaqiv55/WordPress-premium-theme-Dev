f2d205ef8d85cce7b31c0f9d8c69d5344e98e412
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-09-02 02:49:23 -0400

    initial commit

--- wp-content/themes/firsttheme/header.php
+++ wp-content/themes/firsttheme/header.php
@@ -7,7 +7,7 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-
+    <a class="u-skip-link" href="#content"><?php esc_attr_e('Skip to content', '_themename')?></a>
     <header role="banner" class="u-margin-bottom-40"> 
         <div class="c-header">
             <div class="o-container u-flex u-align-justify u-align-middle">
