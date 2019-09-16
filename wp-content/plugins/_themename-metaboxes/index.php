c6833c4e04943fe62d11206b199803d921837316
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-09-16 02:19:26 -0400

    initial commit

---
+++ wp-content/plugins/_themename-metaboxes/index.php
@@ -0,0 +1,19 @@
+<?php 
+/*
+Plugin Name:    _themename _pluginname
+Plugin URI: 
+Description:    Adding Metaboxes for _themename
+Version:        1.0.0
+Author:         Alamgir
+Author:         alamgir.online
+License:        GPL2
+License URI:    someurl.com
+Text Domain:    _themename-_pluginname
+Domain Path:    /language
+*/
+if(!defined('WPINC')){
+    die;
+}
+
+include_once('includes/metaboxes.php');
+include_once('includes/enqueue-assets.php');
---
+++ wp-content/themes/firsttheme/lib/include-plugins.php
@@ -0,0 +1,3 @@
+<?php 
+
+require_once get_template_directory_uri() . '/lib/class-tgm-plugin-activation.php';
